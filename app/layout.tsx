b384e3ea981003486b09c03b2096d1789de2ef49
Harjaap Dhillon <harvydhillon16@gmail.com>
2026-03-09 15:11:55 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script'
 import { IBM_Plex_Mono, Manrope, Space_Grotesk, Cormorant_Garamond } from 'next/font/google'
 
 const display = Space_Grotesk({
@@ -36,6 +37,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-PX8K1QBXY1"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PX8K1QBXY1');
+          `}
+        </Script>
+      </head>
       <body className={`${display.variable} ${body.variable} ${mono.variable} ${serif.variable} font-body`}>
         {children}
       </body>
